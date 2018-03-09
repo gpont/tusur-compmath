d7e52f96498478787f18e5eefc1216ec048998f5
Evgeniy Guzhikhin <gpont97@gmail.com>
2018-03-09 22:09:41 +0700

    Rewrite euler to es7 class

--- src/js/euler.js
+++ src/js/euler.js
@@ -1,26 +1,34 @@
-'use strict';
+import $ from 'jquery';
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("calc").addEventListener("click", () => {
-        let x0 = parseFloat(document.getElementById("input_x0").value);
-        let xn = parseFloat(document.getElementById("input_xn").value);
-        let n = parseFloat(document.getElementById("input_n").value);
-        let f = eval(document.getElementById("input_f").value);
-        document.getElementById("output").innerText = euler(x0, xn, n, f);
-    });
-});
-
-const nextY = (xi, yi, h, f) => yi + h * f(xi + h/2, yi + h/2 * f(xi, yi));
-
-const cut = (num) => num.toString().padEnd(5).substring(0, 5);
-
-const euler = (x0, xn, n, f) => {
-    let outString = '';
-    const h = (xn - x0) / n;
-    let x = x0;
-    while (x < xn) {
-        outString += `${cut(x)} ${cut(nextY(x, f(x), h, f))}\n`;
-        x += h;
+export default class Euler {
+    constructor() {
+        $(document).ready(() => {
+            $("#calc").click(() => {
+                let x0 = parseFloat($("#input_x0").val());
+                let xn = parseFloat($("#input_xn").val());
+                let n = parseFloat($("#input_n").val());
+                let f = eval($("#input_f").val());
+                $("#output").innerText = Euler.calc(x0, xn, n, f);
+            });
+        });
     }
-    return outString;
+
+    static nextY(xi, yi, h, f) {
+        return yi + h * f(xi + h / 2, yi + h / 2 * f(xi, yi));
+    }
+
+    static cut(num) {
+        return num.toString().padEnd(5).substring(0, 5);
+    }
+
+    static calc(x0, xn, n, f) {
+        let outString = '';
+        const h = (xn - x0) / n;
+        let x = x0;
+        while (x < xn) {
+            outString += `${Euler.cut(x)} ${Euler.cut(Euler.nextY(x, f(x), h, f))}\n`;
+            x += h;
+        }
+        return outString;
+    };
 };
