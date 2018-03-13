95f3482901000f193828053ac9f19178330ff9fd
Evgeniy Guzhikhin <gpont97@gmail.com>
2018-03-13 14:42:59 +0700

    Add LagrangePolynomial

---
+++ src/js/LagrangePolynomial.js
@@ -0,0 +1,33 @@
+import BaseForm from './BaseForm';
+
+export default class LagrangePolynomial extends BaseForm {
+  constructor() {
+    super('lagrange-polynomial');
+  }
+
+  getInputs(formName) {
+    const inputElements = document.querySelectorAll(`#${formName} form input`);
+    return [
+      parseFloat(inputElements[0].value),
+      inputElements[1].value.replace(' ', '').split(',').map(el => parseFloat(el)),
+      inputElements[2].value.replace(' ', '').split(',').map(el => parseFloat(el)),
+    ];
+  }
+
+  calc(x, xValues, yValues) {
+    let lagrangePol = 0;
+
+    for (let i = 0; i < xValues.length; ++i) {
+      let basicsPol = 1;
+      for (let j = 0; j < xValues.length; ++j) {
+        if (j !== i) {
+          basicsPol *= (x - xValues[j]) / (xValues[i] - xValues[j]);
+        }
+      }
+
+      lagrangePol += basicsPol * yValues[i];
+    }
+
+    return lagrangePol;
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,10 @@ import Euler from './Euler';
 import TabsManager from './TabsManager';
 import CauchyAdams4 from './IntLeftRectangle';
 import NewtonPolinomial from './NewtonPolynomial';
+import LagrangePolinomial from './LagrangePolynomial';
 
 new TabsManager();
 new Euler();
 new CauchyAdams4();
 new NewtonPolinomial();
+new LagrangePolinomial();
