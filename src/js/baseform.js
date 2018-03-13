4ef333caf53b308118e4574f54cea20457550362
Evgeniy Guzhikhin <gpont97@gmail.com>
2018-03-13 14:06:56 +0700

    Add getInputs in BaseForm

--- src/js/baseform.js
+++ src/js/baseform.js
@@ -1,24 +1,29 @@
 export default class BaseForm {
   constructor(formName) {
-    document.addEventListener('DOMContentLoaded', BaseForm.page.bind(this, formName));
+    document.addEventListener('DOMContentLoaded', () => this.page(formName));
   }
 
-  static page(formName) {
+  page(formName) {
     const form = document.querySelector(`#${formName} form`);
-    const inputs = document.querySelectorAll(`#${formName} form input[type="number"]`);
     const output = document.querySelector(`#${formName} pre`);
+    const clearButton = document.querySelector(`#${formName} button[type="button"]`);
 
     form.addEventListener('submit', (event) => {
       event.preventDefault();
-
-      const args = Array.from(inputs).map(el => parseFloat(el.value));
-      // eslint-disable-next-line no-eval
-      args.push(eval(form.querySelector('input[type="text"]').value));
-      output.innerText = this.calc(...args);
+      output.innerText = this.calc(...this.getInputs(formName));
     });
-    form.querySelector('button[type="button"]').addEventListener('click', () => {
+    clearButton.addEventListener('click', () => {
       output.innerText = '';
     });
+  }
+
+  getInputs(formName) {
+    const funcInput = document.querySelector(`#${formName} input[type="text"]`);
+    const inputElements = document.querySelectorAll(`#${formName} form input[type="number"]`);
+    const inputs = Array.from(inputElements).map(el => parseFloat(el.value));
+    // eslint-disable-next-line no-eval
+    inputs.push(eval(funcInput.value));
+    return inputs;
   }
 
   calc() {}
