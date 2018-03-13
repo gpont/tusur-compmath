d45eaa63d7d22636ea6b019285d4849450b5cc76
Evgeniy Guzhikhin <gpont97@gmail.com>
2018-03-13 14:08:25 +0700

    Add NewtonPolynomial

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Euler from './euler';
 import TabsManager from './tabs';
 import CauchyAdams4 from './cauchy_adams_4';
+import NewtonPolinomial from './NewtonPolynomial';
 
 new TabsManager();
 new Euler();
 new CauchyAdams4();
+new NewtonPolinomial();
